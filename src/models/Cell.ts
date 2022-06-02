e36e3b0e62066652315313f01b3695094ae8f13b
maksim <krovorgen@gmail.com>
2022-06-02 19:15:24 +0300

    fixed

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -3,13 +3,13 @@ import { Figure } from './figures/Figure';
 import { Board } from './Board';
 
 export class Cell {
+  public available: boolean = false; // is it possible to move
+  public id: number = Math.random();
   constructor(
     public board: Board,
     readonly x: number,
     readonly y: number,
     readonly color: Colors,
-    public figure: Figure | null,
-    public available: boolean = false, // is it possible to move
-    public id: number = Math.random(), // for react key
+    public figure: Figure | null, // for react key
   ) {}
 }
