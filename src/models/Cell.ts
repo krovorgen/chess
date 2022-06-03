8599b85989e5213cc88345ce7af8f6394569e158
maksim <krovorgen@gmail.com>
2022-06-03 09:58:34 +0300

    fix

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -20,7 +20,7 @@ export class Cell {
   isEmptyVertical(target: Cell): boolean {
     if (this.x !== target.x) return false;
     const min = Math.min(this.y, target.y);
-    const max = Math.min(this.y, target.y);
+    const max = Math.max(this.y, target.y);
     for (let y = min + 1; y < max; y++) {
       if (!this.board.getCell(this.x, y).isEmpty()) {
         return false;
@@ -33,7 +33,7 @@ export class Cell {
     if (this.y !== target.y) return false;
 
     const min = Math.min(this.x, target.x);
-    const max = Math.min(this.x, target.x);
+    const max = Math.max(this.x, target.x);
     for (let x = min + 1; x < max; x++) {
       if (!this.board.getCell(x, this.y).isEmpty()) {
         return false;
